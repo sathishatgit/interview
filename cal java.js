4b5ab562865b753baae3588c7463de50a8d7ba86
sathishatgit <103997288+sathishatgit@users.noreply.github.com>
2022-10-28 15:26:55 +0530

    Calculator Web Application

---
+++ cal java.js
@@ -0,0 +1,37 @@
+alert(`Dear User:-
+        Single Click on C Remove One Data,If You Want To Remove Whole Data Please Click Double Time On C
+                                     
+                                     Thanks For Choosing Our Site
+                                            Happy Learning`);
+function fun(x)
+{
+    if(x!='c')
+    document.getElementById("show").value+=x;
+    else
+    document.getElementById("show").value='';
+    console.log(typeof(document.getElementById("show").value));
+}
+function remove()
+{
+    document.getElementById("show").value=((document.getElementById("show").value).slice(0,-1));
+}
+function total(val)
+{
+    return new Function('return ' + val)();
+}
+function sum(x)
+{
+    try{
+    let a=total(document.getElementById("show").value)
+    console.log(typeof(a));
+    document.getElementById("show").value=a;
+    }
+    catch(e)
+    {
+    alert("Check Your Input");
+    }
+}
+function plusminus()
+{
+        document.getElementById("show").value*=-1;
+}
